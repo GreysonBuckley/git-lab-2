937fb0c366d864a3f8312a408c89b6390d84f996
GreysonBuckley <buckleygrey@gmail.com>
2025-09-05 15:19:26 -0400

    Pushing git-lab-program.cc to GitHub.

---
+++ git-lab-program.cc
@@ -0,0 +1,16 @@
+/*
+*            File: git-lab-program.cc
+*            Author: Greyson Buckley
+*            Date: 9/5/25
+*            Description: This program will output "Hello Git!!"
+*/ 
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+    cout << "Hello Git!!" << endl;
+    return 0;
+} // main
